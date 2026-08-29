2274b4823d461b183b3086d2b390ad4bea65a285
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-29 21:37:00 +0530

    Create universe.py

---
+++ src/data/universe.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+class Universe:
+
+    def __init__(self):
+
+        self.stocks = pd.read_parquet("data/stocks.parquet")
+        self.indices = pd.read_parquet("data/indices.parquet")
+
+    def get_indices(self):
+
+        wanted = [
+            "NIFTY 50",
+            "NIFTY BANK",
+            "NIFTY FIN SERVICE",
+            "NIFTY MIDCAP SELECT",
+            "SENSEX",
+            "BANKEX"
+        ]
+
+        return self.indices[
+            self.indices["trading_symbol"].isin(wanted)
+        ]
+
+    def get_fno_stocks(self):
+
+        return self.stocks
