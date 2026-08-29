5bc454f3f5c282bfdc44b0289e789c5f11a66518
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-29 21:38:00 +0530

    Create cache.py

---
+++ src/storage/cache.py
@@ -0,0 +1,19 @@
+class MarketCache:
+
+    def __init__(self):
+
+        self.live_quotes = {}
+
+        self.candles = {}
+
+        self.market = {}
+
+        self.sectors = {}
+
+        self.rs = {}
+
+        self.volume = {}
+
+        self.structure = {}
+
+        self.smart_money = {}
