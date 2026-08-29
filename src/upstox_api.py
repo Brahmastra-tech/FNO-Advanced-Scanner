e12bdcb56061a3d1e33c7e55b41fc697fc186a6f
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-29 17:11:07 +0530

    Create upstox_api.py

---
+++ src/upstox_api.py
@@ -0,0 +1,10 @@
+class UpstoxAPI:
+
+    def __init__(self):
+        print("Upstox API Ready")
+
+    def login(self):
+        print("Login Pending")
+
+    def fetch_market_quote(self):
+        print("Market Quote Pending")
