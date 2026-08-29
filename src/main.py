5b1e206315ad20bd13170ff3720eacd2c7deb4cf
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-29 14:57:56 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.auth import UpstoxAuth
+
+def main():
+
+    auth = UpstoxAuth()
+
+    print("Project Started")
+
+    print("Authentication Module Loaded")
+
+if __name__ == "__main__":
+    main()
