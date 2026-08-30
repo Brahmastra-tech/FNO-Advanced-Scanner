41d5d118fb2b93c6aa9c1abea5c6655ac79d57a6
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-30 13:28:37 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
-from src.upstox_api import UpstoxAPI
-
-
-def main():
-    api = UpstoxAPI()
-
-    quote = api.get_quote("NSE_INDEX|Nifty 50")
-
-    print(quote)
-
+from engine.pipeline import Pipeline
 
 if __name__ == "__main__":
-    main()
+
+    pipeline = Pipeline()
+
+    pipeline.run()
