30fabd30888d99a567f2dd31f53ea3de8d32cce6
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-31 10:58:25 +0530

    Update universe.py

--- src/data/universe.py
+++ src/data/universe.py
@@ -8,6 +8,19 @@ class Universe:
         self.stocks = pd.read_parquet("data/stocks.parquet")
         self.indices = pd.read_parquet("data/indices.parquet")
 
+        # Load NIFTY 500 list
+        nifty500 = pd.read_csv("data/nifty500.csv")
+
+        # Convert symbols to uppercase and remove spaces
+        nifty500["Symbol"] = (
+            nifty500["Symbol"]
+            .astype(str)
+            .str.upper()
+            .str.strip()
+        )
+
+        self.nifty500_symbols = set(nifty500["Symbol"])
+
     def get_indices(self):
 
         wanted = [
@@ -25,4 +38,19 @@ class Universe:
 
     def get_fno_stocks(self):
 
-        return self.stocks
+        stocks = self.stocks.copy()
+
+        stocks["trading_symbol"] = (
+            stocks["trading_symbol"]
+            .astype(str)
+            .str.upper()
+            .str.strip()
+        )
+
+        stocks = stocks[
+            stocks["trading_symbol"].isin(self.nifty500_symbols)
+        ]
+
+        print(f"NIFTY 500 Universe Loaded: {len(stocks)} stocks")
+
+        return stocks
