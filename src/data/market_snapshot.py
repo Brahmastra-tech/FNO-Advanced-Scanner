f63d246225152cb02722d030e5904f5961a292f1
Brahmastra-tech <mediahiddenframe@gmail.com>
2026-08-31 12:47:29 +0530

    Update market_snapshot.py

--- src/data/market_snapshot.py
+++ src/data/market_snapshot.py
@@ -28,7 +28,7 @@ class MarketSnapshot:
                 if not data_dict:
                     continue
 
-                # Upstox returns one quote per response.
+                # Upstox returns one quote inside the "data" dict.
                 # The key may be "NSE_EQ:MARUTI" instead of the instrument key.
                 data = next(iter(data_dict.values()))
 
@@ -42,52 +42,29 @@ class MarketSnapshot:
             depth = data.get("depth", {})
 
             row = {
-
                 "timestamp": datetime.now(),
-
                 "instrument_key": instrument_key,
-
                 "symbol": data.get("symbol"),
-
                 "exchange": data.get("exchange"),
-
                 "ltp": data.get("last_price"),
-
                 "open": ohlc.get("open"),
-
                 "high": ohlc.get("high"),
-
                 "low": ohlc.get("low"),
-
                 "close": ohlc.get("close"),
-
                 "volume": data.get("volume"),
-
                 "turnover": data.get("turnover"),
-
                 "oi": data.get("oi"),
-
                 "prev_oi": data.get("prev_oi"),
-
                 "avg_price": data.get("average_price"),
-
                 "last_trade_time": data.get("last_trade_time"),
-
                 "total_buy_qty": data.get("total_buy_quantity"),
-
                 "total_sell_qty": data.get("total_sell_quantity"),
-
                 "upper_circuit": data.get("upper_circuit_limit"),
-
                 "lower_circuit": data.get("lower_circuit_limit"),
-
                 "bid": None,
-
                 "bid_qty": None,
-
                 "ask": None,
-
-                "ask_qty": None
+                "ask_qty": None,
             }
 
             # Best Bid
